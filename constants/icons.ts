29d235bb214fd35c3c2bd2dfa8c3e8e87e8c8b29
ss889 <98911705+ss889@users.noreply.github.com>
2025-02-18 18:11:56 -0500

    Add files via upload

---
+++ constants/icons.ts
@@ -0,0 +1,15 @@
+import home from "../assets/icons/home.png";
+import menu from "../assets/icons/menu.png";
+import cart from "./../assets/icons/cart.png";
+import logo from "./../assets/icons/toc-logo.png";
+import pin from "./../assets/icons/pin.png";
+import welcome from "./../assets/icons/welcome.png";
+
+export default {
+  home,
+  menu,
+  cart,
+  logo,
+  pin,
+  welcome
+};
---
+++ constants/images.ts
@@ -0,0 +1,47 @@
+import ice_cream from "./../assets/images/Menu/Home_Made_Ice_Cream.jpeg"
+import cheesecake from "./../assets/images/Menu/Passion_Fruit_Cheesecake.jpeg"
+import creme_brulee from "./../assets/images/Menu/Ginger_Spiced_Creme_Brulee.jpeg"
+import black_cake from "./../assets/images/Menu/West_Indian_Black_Cake.jpeg"
+import pudding from "./../assets/images/Menu/Mom_Bread_Pudding.jpeg"
+import green_plantains from "../assets/images/Menu/Fried_Green_Plantains.jpeg"
+import mac from "./../assets/images/Menu/Mac_N_Cheese.jpeg"
+import jasmine from "./../assets/images/Menu/Jasmine_Rice.jpeg"
+import yard from "./../assets/images/Menu/Yard_Food.jpeg"
+import curry from "./../assets/images/Menu/Curry_Lobster_Shrimp_and_Salmon.jpeg"
+import shrimp from "./../assets/images/Menu/Wild_Caught_Shrimp.jpeg"
+import salmon from "./../assets/images/Menu/Herb_Salmon.jpeg"
+import spring_roll from "./../assets/images/Menu/Spring_Roll.jpeg"
+import indian_soup from "./../assets/images/Menu/West_Indian_Soup.jpeg"
+import tropical_salad from "./../assets/images/Menu/Mini_Tropical_Salad.jpeg"
+import jerk_salmon from "./../assets/images/Menu/Jerk_Salmon.jpeg"
+import jerk_chicken from "./../assets/images/Menu/Jerk_Chicken.jpeg"
+import pork_skewer from "./../assets/images/Menu/Jerk_Pork_Skewer.jpeg"
+import empanadas from "./../assets/images/Menu/Empanadas.jpeg"
+import codfish from "./../assets/images/Menu/Codfish_Fritters.jpeg"
+import fried_plantains from "./../assets/images/Menu/Sweet_Plantains.jpeg"
+
+export default {
+    ice_cream,
+    cheesecake,
+    creme_brulee,
+    black_cake,
+    pudding,
+    green_plantains,
+    mac,
+    jasmine,
+    yard,
+    curry,
+    shrimp,
+    salmon,
+    spring_roll,
+    indian_soup,
+    tropical_salad,
+    jerk_salmon,
+    jerk_chicken,
+    pork_skewer,
+    empanadas,
+    codfish,
+    fried_plantains
+
+
+};
